39879df2807a4a8ed0de911f88de0b7abf16b304
Stefano Trepella <stefanotrepella@gmail.com>
2025-04-03 16:44:27 +0200

    added descriptions for functions in cluster evaluation and modified function in notebook to include normalize parameter

--- social_metrics_match/utils/cluster_evaluation.py
+++ social_metrics_match/utils/cluster_evaluation.py
@@ -1,5 +1,6 @@
 from sklearn.decomposition import PCA
 import pandas as pd
+import numpy as np
 
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
@@ -8,40 +9,65 @@ from sklearn.cluster import DBSCAN
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from sklearn.metrics import silhouette_score, adjusted_rand_score, confusion_matrix
 
-def cluster_comparison_using_ARI(array_of_features_,use_pca=True,n_clusters=3, name='array'):
+def cluster_comparison_using_ARI(labels_to_analyze : np.array, name='array'):
     """
-    Given the array of features, executes clusering using two techniques: k-means and hierarchical clustering.
+    Given the labels fed as input, computes the adjusted rand score
     Computes ARI which shows how closely the cluster labels match the preassigned labels
     """
-    if use_pca:
-        pca = PCA(n_components=0.95)
-        array_of_features = pca.fit_transform(array_of_features_)
-    else:
-        array_of_features = array_of_features_
-    cluster_labels_k_means = cluster_K_means(array_of_features, n_clusters=n_clusters )    
-    cluster_labels_linkage = cluster_hierarchical_K_means(array_of_features, method='complete')  # or 'complete', 'average', etc.
 
-    manual_labels=["Bad","Intermediate","Good","Good","Intermediate","Bad","Intermediate","Bad","Good","Intermediate","Good","Bad",
+    manual_labels=["Bad","Intermediate","Good","Good","Intermediate","Bad","Intermediate","Bad","Good","Good","Intermediate","Bad",
                    "Bad","Intermediate","Good","Good","Intermediate","Bad","Intermediate","Bad","Good","Intermediate","Good","Bad"]
     manual_encoded = pd.Categorical(manual_labels, categories=["Bad", "Intermediate", "Good"]).codes
 
-    ari_linkage = adjusted_rand_score(manual_encoded, cluster_labels_linkage)
-    print(f"Adjusted Rand Index hierarchical for {name}: {ari_linkage:.2f}")
-    ari_k_means = adjusted_rand_score(manual_encoded, cluster_labels_k_means)
-    print(f"Adjusted Rand Index k-means for {name}: {ari_k_means:.2f}")
-    return ari_k_means, ari_linkage
+    ari_labels = adjusted_rand_score(manual_encoded, labels_to_analyze)
+    print(f"Adjusted Rand Index k-means for {name}: {ari_labels:.2f}")
 
-def cluster_K_means(arr, K=3):
+    return ari_labels
+
+def cluster_K_means(arr : np.array, K=3):
+    """
+    Computes the k-means clustering in K-clusters, output is as an array with the assigned labels
+    Input: array of samples to cluster
+    Output: array of labels assigned
+    """
     kmeans= KMeans(n_clusters=K, random_state=42, init='k-means++')
     cluster_labels = kmeans.fit_predict(arr)
     return cluster_labels
 
-def cluster_hierarchical_K_means(arr, K=3):
+def cluster_hierarchical_K_means(arr : np.array, K=3):
+    """
+    Computes the hierarchical clustering in K-clusters, output is as an array with the assigned labels
+
+    Input: array of samples to cluster
+
+    Output: array of labels assigned
+    """
     Z = linkage(arr, method='complete')  # or 'complete', 'average', etc.
     cluster_labels = fcluster(Z, t=K, criterion='maxclust')
     return cluster_labels
 
-def run_TSNE(arr, n=2, p=6):
+def run_TSNE(arr : np.array, n=2, p=6):
+    """
+    Reduces the dimensionality of input array to n-dimensions using t-SNE
+
+    Input: array of samples to be considered
+
+    Output: array of samples with n-features
+    """
     tsne_2d = TSNE(n_components = n, random_state=42, perplexity=p)
     tsne_results_2d = tsne_2d.fit_transform(arr)
     return tsne_results_2d
+
+def run_PCA(arr: np.array, n_components: int = 2):
+    """
+    Reduces the dimensionality of the input array to n_components using PCA.
+    
+    Input:
+      - arr: array of samples to be considered
+      - n_components: the number of principal components to keep
+
+    Output: array of samples with n_components features
+    """
+    pca = PCA(n_components= n_components, random_state=42)
+    pca_results = pca.fit_transform(arr)
+    return pca_results
